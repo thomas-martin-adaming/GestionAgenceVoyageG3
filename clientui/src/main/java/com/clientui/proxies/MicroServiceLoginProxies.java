a68c10d1e1cbae5ca44de84c5c46321c445b6d43
thomas-martin-adaming <gmartinthomas7@gmail.com>
2021-01-26 16:16:01 +0100

    supp login ui

--- clientui/src/main/java/com/clientui/proxies/MicroServiceLoginProxies.java
+++
@@ -1,8 +0,0 @@
-package com.clientui.proxies;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "zuul-server", url = "localhost:9004/")
-public interface MicroServiceLoginProxies {
-	
-}
